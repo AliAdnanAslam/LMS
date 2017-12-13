dd40aa4a422c9b14b6c3c5fb486cdd72cb7c40f5
AliAdnanAslam <aliadnanaslam226@gmail.com>
2017-12-13 19:03:54 +0500

    ADD: Labels to all input fields

--- client/src/components/admin/ModifyBook.js
+++ client/src/components/admin/ModifyBook.js
@@ -25,46 +25,50 @@ class ModifyBook extends Component {
                                     <div class="module-body">
                                         <div class="control-group">
                                             <div class="controls row-fluid">
+                                                <label class="control-label">Name</label>
                                                 <input class="span12" type="text" id="bookName" placeholder="Book Name" required />
                                             </div>
                                         </div>
                                         <div class="control-group">
                                             <div class="controls row-fluid">
+                                                <label class="control-label">Author Name</label>
                                                 <input class="span12" type="text" id="authorName" placeholder="Author Name" required />
                                             </div>
                                         </div>
                                         <div class="control-group">
                                             <div class="controls row-fluid">
+                                                <label class="control-label">Edition</label>
                                                 <input class="span12" type="text" id="edition" placeholder="Edition" required />
                                             </div>
                                         </div>
                                         <div class="control-group">
                                             <div class="controls row-fluid">
+                                                <label class="control-label">Publication</label>
                                                 <input class="span12" type="text" id="publication" placeholder="Publication" required />
                                             </div>
                                         </div>
                                         <div class="control-group">
                                             <div class="controls row-fluid">
+                                                <label class="control-label">Publication Year</label>
                                                 <input class="span12" type="text" id="publicationYear" placeholder="Publication Year" required />
                                             </div>
                                         </div>         
                                         <div class="control-group">
                                             <div class="controls row-fluid">
+                                                <label class="control-label">Quantity</label>
                                                 <input class="span12" type="text" id="quantity" placeholder="Quantity" required />
                                             </div>
                                         </div>                                                                                                              
                                     </div>
                                     <div style={{marginLeft: '20px'}}>
-                                        <label class="control-label"><b>Select Image From Your Computer</b></label>
+                                        <label class="control-label">Select Image From Your Computer</label>
                                         <input type="file" class="file" accept="image/*" required/>
                                         <br /><br />            
                                     </div>
                                     <div class="module-foot">
                                         <div class="control-group">
                                             <div class="controls clearfix">
-                                                <button type="" class="btn btn-primary pull-right">Cancel</button>
-                                                <button type="submit" class="btn btn-primary pull-right" style={{marginRight: '10px'}}>OK</button>
-                                                <button type="" class="btn btn-primary pull-left">Remove Book</button>                                
+                                                <button type="" class="btn btn-primary pull-right">Submit</button>                                                
                                             </div>
                                         </div>
                                     </div>
